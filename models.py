377f3c609c4db536982be6cba4733f9080c0c981
Juliano Fernandes Baeta <baeta.juliano@gmail.com>
2017-08-27 17:17:55 -0300

    v0.2.1 - models.py bug fixes.

--- models.py
+++ models.py
@@ -1,1 +1,25 @@
 # -*- coding: UTF-8 -*-
+
+class PhysicalVolume(object):
+	"""class PhysicalVolume"""
+	def __init__(self, arg):
+		super(PhysicalVolume, self).__init__()
+		self.arg = arg
+
+class VolumeGroup(object):
+	"""class VolumeGroup"""
+	def __init__(self, arg):
+		super(VolumeGroup, self).__init__()
+		self.arg = arg
+
+class LogicalVolume(object):
+	"""class LogicalVolume"""
+	def __init__(self, arg):
+		super(LogicalVolume, self).__init__()
+		self.arg = arg
+
+class FileSystem(object):
+	"""class FileSystem"""
+	def __init__(self, arg):
+		super(FileSystem, self).__init__()
+		self.arg = arg
