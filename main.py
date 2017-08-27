0ed737562a0d766d9962837605280bf534754039
Juliano Fernandes Baeta <baeta.juliano@gmail.com>
2017-08-27 15:36:32 -0300

    v0.1.5 - Bug fixes.

--- main.py
+++ main.py
@@ -1,28 +1,115 @@
 # -*- coding: UTF-8 -*-
 
 def create_pvs(pvs):
-	print 'Type Physical Volume name:'
-	pv = raw_input()
-	pvs.append(pv)
+	print 'Type the amount of Physical Volumes for Data:',
+	pv_amount = int(raw_input())
+
+	print 'Type Physical Volume name prefix for Data:',
+	pv_prefix = raw_input()
+
+	pv_count = 1
+	while (pv_count <= pv_amount):
+		pv_suffix = str(pv_count)
+		pv_name = pv_prefix + pv_suffix
+		pvs.append(pv_name)
+		print 'Creating Physical Volume %s...' % (pv_name)
+		pv_count+=1
+
+	print 'Type the amount of Physical Volumes for Log:',
+	pv_amount = int(raw_input())
+
+	print 'Type Physical Volume name prefix for Log:',
+	pv_prefix = raw_input()
+
+	pv_count = 1
+	while (pv_count <= pv_amount):
+		pv_suffix = str(pv_count)
+		pv_name = pv_prefix + pv_suffix
+		pvs.append(pv_name)
+		print 'Creating Physical Volume %s...' % (pv_name)
+		pv_count+=1
+
+	print 'Type the amount of Physical Volumes for Shared:',
+	pv_amount = int(raw_input())
+
+	print 'Type Physical Volume name prefix for Shared:',
+	pv_prefix = raw_input()
+
+	pv_count = 1
+	while (pv_count <= pv_amount):
+		pv_suffix = str(pv_count)
+		pv_name = pv_prefix + pv_suffix
+		pvs.append(pv_name)
+		print 'Creating Physical Volume %s...' % (pv_name)
+		pv_count+=1
+
+	exit()
 
 def create_vgs(vgs):
-	print 'Type Volume Group name:'
+	print 'Type Volume Group name for Data:',
 	vg = raw_input()
 	vgs.append(vg)
+	print 'Volume Group for Data will be %s' % (vg)
+
+	print 'Type Volume Group name for Log:',
+	vg = raw_input()
+	vgs.append(vg)
+	print 'Volume Group for Log will be %s' % (vg)
+
+	print 'Type Volume Group name for Shared:',
+	vg = raw_input()
+	vgs.append(vg)
+	print 'Volume Group for Shared will be %s' % (vg)
+
+	print 'Creating Volume Group %s...' % (vgs[0])
+	print 'Creating Volume Group %s...' % (vgs[1])
+	print 'Creating Volume Group %s...' % (vgs[2])
+
+	exit()
 
 def create_lvs(lvs):
-	print 'Type Logical Volume name:'
+	print 'Type Logical Volume name for Data:',
 	lv = raw_input()
 	lvs.append(lv)
+	print 'Logical Volume for Data will be %s' % (lv)
+
+	print 'Type Logical Volume name for Log:',
+	lv = raw_input()
+	lvs.append(lv)
+	print 'Logical Volume for Log will be %s' % (lv)
+
+	print 'Type Logical Volume name for Shared:',
+	lv = raw_input()
+	lvs.append(lv)
+	print 'Logical Volume for Shared will be %s' % (lv)
+
+	print 'Creating Logical Volume %s...' % (lvs[0])
+	print 'Creating Logical Volume %s...' % (lvs[1])
+	print 'Creating Logical Volume %s...' % (lvs[2])
+
+	exit()
 
 def create_fss(fss):
-	print 'Type File System name:'
+	print 'Type File System name for Data:',
 	fs = raw_input()
 	fss.append(fs)
-	print fss
-	print 'Creating File System /hana/data...'
-	print 'Creating File System /hana/log...'
-	print 'Creating File System /hana/shared...'
+	print 'File System for Data will be %s.' % (fs)
+	
+	print 'Type File System name for Log:',
+	fs = raw_input()
+	fss.append(fs)
+	print 'File System for Log will be %s.' % (fs)
+
+	print 'Type File System name for Shared:',
+	fs = raw_input()
+	fss.append(fs)
+	print 'File System for Shared will be %s.' % (fs)
+
+	print 'Creating File System %s...' % (fss[0])
+	print 'Creating File System %s...' % (fss[1])
+	print 'Creating File System %s...' % (fss[2])
+
+	exit()
 
 def menu():
 
@@ -34,33 +121,23 @@ def menu():
 	option =''
 	
 	while(option != 'Q' and option != 'q' and option != 'E' and option != 'e'):
-		print 'Create Physical Volumes (1):'
-		print 'Create Volume Groups (2):'
-		print 'Create Logical Volumes (3):'
-		print 'Create File Systems (4):'
-		print 'Quit/Exit (Q,q,E,e):'
+		print 'Create Physical Volumes (1)'
+		print 'Create Volume Groups (2)'
+		print 'Create Logical Volumes (3)'
+		print 'Create File Systems (4)'
+		print 'Quit/Exit (Q,q,E,e)'
+		print 'Choose your Option:',
 		option = raw_input()
 
 		if(option == '1'):
 			create_pvs(pvs)
-			print 'Creating Physical Volume hana_data_01...'
-			print 'Creating Physical Volume hana_data_02...'
-			print 'Creating Physical Volume hana_data_03...'
-			print 'Creating Physical Volume hana_data_04...'
-			print 'Creating Physical Volume hana_log_01...'
-			print 'Creating Physical Volume hana_shared_01...'
 
 		if(option == '2'):
 			create_vgs(vgs)
-			print 'Creating Volume Group hanadatavg...'
-			print 'Creating Volume Group hanalogvg...'
-			print 'Creating Volume Group hanasharedvg...'
+
 
 		if(option == '3'):
 			create_lvs(lvs)
-			print 'Creating Logical Volume lv_hana_data...'
-			print 'Creating Logical Volume lv_hana_log...'
-			print 'Creating Logical Volume lv_hana_shared...'
 
 		if(option == '4'):
 			create_fss(fss)
