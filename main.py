34e446567778d1a91715d9504deaf71797d82fd1
Juliano Fernandes Baeta <baeta.juliano@gmail.com>
2017-08-31 17:45:34 -0300

    v0.2.6 - detect_luns() fix.

--- main.py
+++ main.py
@@ -13,142 +13,60 @@ def detect_lun_amount():
 
 def detect_luns():
 
+	luns = []
+
 	file = open('multi-grep.txt', 'r')
 	regex_name = re.compile(r'(\w+)(?:\s\()')
 	names = re.findall(regex_name, file.read())
 	file.close()
-	print names
-	for name in names:
-		print name
 
 	file = open('multi-grep.txt', 'r')
 	regex_uuid = re.compile(r'\w{33}')
 	uuids = re.findall(regex_uuid, file.read())
 	file.close()
-	print uuids
-	for uuid in uuids:
-		print uuid
 
 	file = open('multi-grep.txt', 'r')
 	regex_sysfs = re.compile(r'dm-\d{1,3}')
 	sysfss = re.findall(regex_sysfs, file.read())
 	file.close()
-	print sysfss
-	for sysfs in sysfss:
-		print sysfs
 
 	file = open('multi-grep.txt', 'r')
 	regex_vendor = re.compile(r'(\w+)(?:,)')
 	vendors = re.findall(regex_vendor, file.read())
 	file.close()
-	print vendors
-	for vendor in vendors:
-		print vendor
 
 	file = open('multi-grep.txt', 'r')
 	regex_product = re.compile(r'(?:,)(\w+)')
 	products = re.findall(regex_product, file.read())
 	file.close()
-	print products
-	for product in products:
-		print product
 
 	file = open('multi-grep.txt', 'r')
 	regex_size_n = re.compile(r'(?:size=)(\d+\.?\d*?)')
 	sizes_n = re.findall(regex_size_n, file.read())
 	file.close()
-	print sizes_n
-	for size_n in sizes_n:
-		print size_n
 
 	file = open('multi-grep.txt', 'r')
 	regex_size_m = re.compile(r'([MGT])(?:[\s])')
 	sizes_m = re.findall(regex_size_m, file.read())
 	file.close()
-	print sizes_m
-	for size_m in sizes_m:
-		print size_m
 
-def detect_pvs():
+	lun_list_count = len(names)
+	lun_list_index=0
+	while lun_list_index < lun_list_count:
+		name = names[lun_list_index]
+		uuid = uuids[lun_list_index]
+		sysfs = sysfss[lun_list_index]
+		vendor = vendors[lun_list_index]
+		product = products[lun_list_index]
+		size_n = sizes_n[lun_list_index]
+		size_m = sizes_m[lun_list_index]
+		luns.append(PhysicalVolume(name=name, uuid=uuid, sysfs=sysfs, vendor=vendor, product=product, size_n=size_n, size_m=size_m))
+		lun_list_index+=1
 
-	luns = []
-	lun_dict = {}
-	print 'Detecting Multipath LUNs...'
-	
-	file = open('multi-grep.txt', 'r')
-
-	regex_name = re.compile(r'(^\w+)(?:\s)')
-	regex_uuid = re.compile(r'\w{33}')
-	regex_sysfs = re.compile(r'dm-\d{1,3}')
-	regex_vendor = re.compile(r'(\w+)(?:,)')
-	regex_product = re.compile(r'(?:,)(\w+)')
-	regex_size_n = re.compile(r'(?:size=)(\d+\.?\d*?)')
-	regex_size_m = re.compile(r'([MGT])(?:[\s])')
-	regex_discard = re.compile(r'(\:|\+)')
-
-	for line in file:
-
-		name = re.findall(regex_name, line)
-		if name:
-			name = str(name[0])
-			lun_dict['name_key'] = name
-			
-		uuid = re.findall(regex_uuid, line)
-		if uuid:
-			uuid = str(uuid[0])
-			lun_dict['uuid_key'] = uuid
-			
-		sysfs = re.findall(regex_sysfs, line)
-		if sysfs:
-			sysfs = str(sysfs[0])
-			lun_dict['sysfs_key'] = sysfs
-			
-		vendor = re.findall(regex_vendor, line)
-		if vendor:
-			vendor = str(vendor[0])
-			lun_dict['vendor_key'] = vendor
-			
-		product = re.findall(regex_product, line)
-		if product:
-			product = str(product[0])
-			lun_dict['product_key'] = product
-			
-		size_n = re.findall(regex_size_n, line)
-		if size_n:
-			size_n = str(size_n[0])
-			lun_dict['size_n_key'] = size_n
-			
-		size_m = re.findall(regex_size_m, line)
-		if size_m:
-			size_m = str(size_m[0])
-			lun_dict['size_m_key'] = size_m
-			
-		if len(lun_dict) == 7:
-			luns.append(PhysicalVolume(name=name, uuid=uuid, sysfs=sysfs, vendor=vendor, product=product, size_n=size_n, size_m=size_m))
-
-	for i in luns:
-		i.show_lun_name()
-
-	#luns.append(PhysicalVolume(name=name, uuid=uuid, wwid='', sysfs=sysfs, vendor=vendor, product=product, size_n=size_n, size_m=size_m))
-
-		#lun = PhysicalVolume(name=name, uuid=uuid, wwid='', sysfs=sysfs, vendor=vendor, product=product, size_n=size_n, size_m=size_m)
-		#print lun.name
-		#print lun.uuid
-		#print lun.sysfs
-		#print lun.vendor
-		#print lun.product
-		#print lun.size_n
-		#print lun.size_m
-		#luns.append(lun)
-
-		#print 'Nome da Lun: %s' % (name[0])
-		#print 'UUID da Lun: %s' % (uuid[0])
-		#print 'DM da Lun: %s' % (sysfs[0])
-		#print 'Vendor do Storage: %s' % (vendor[0])
-		#print 'Modelo do Storage: %s' % (product[0])
-		#print 'Tamanho da Lun: %s%s' % (size_n[0], size_m[0])
-
-	file.close()
+	print luns
+	print luns[0].name
+	print luns[0].uuid
+	print luns[7].sysfs
 
 def create_pvs(pvs):
 	print 'Type the amount of Physical Volumes for Data:',
@@ -300,7 +218,6 @@ def menu():
 			print 'Invalid Option!'
 
 #menu()
-#detect_pvs()
 detect_luns()
 
 
